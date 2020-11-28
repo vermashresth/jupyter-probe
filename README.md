2c17aa161ecf93c0b9f37579918fb5460cb6c186
Shresth Verma <vermashresth@gmail.com>
2020-11-28 18:10:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gpu-share-lab
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="jupyter-probe",
+    version="0.0.1",
+    author="Shresth Verma",
+    author_email="vermashresth@gmail.com",
+    description="A package to monitor, manage, declare and analyse notebook resource usage on jupyter environments",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vermashresth/jupyter-probe",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
